de79a2c8071b90f3df16f986c7175738cbb1c6c0
Lionel Kaneza <lionkaneza@gmail.com>
2023-07-17 11:25:32 +0200

    end-search

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,27 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        main: "Graphik-Regular",
+        "main-medium": "Graphik-Medium",
+        "main-semibold": "Graphik-Semibold",
+        "druk-medium": "Druk-Medium",
+        "publico-text": "PublicoText-Roman",
+      },
+      colors: {
+        "my-yellow": "#FED100",
+        "hover-brown": "#A42017",
+        "gray-text": "#333",
+        "gray-bg": "#2B2B2B",
+        "my-link": "#376ebe",
+        "body-bg": "#F6F7F7"
+      },
+    },
+    container: {
+      center: true
+    }
   },
   plugins: [],
-}
+};
 
